b4a862421599c8c20896deca7ea4651a06c4d5ca
Toshihide-Sakao <toshihide.sakao@elev.ga.ntig.se>
2020-10-14 09:58:19 +0200

    Provstart

---
+++ Prov1-Toshihide-Sakao/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Prov1_Toshihide_Sakao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
